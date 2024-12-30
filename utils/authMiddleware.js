62ae6dcc68a6e7288b676b23a64e463655edc522
jibrantf <brandozzzz05@gmail.com>
2024-12-30 08:46:10 +0700

    Update authMiddleware.js

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -16,18 +16,16 @@ exports.authenticateJWT = (req, res, next) => {
             return res.status(401).json({ message: 'Token tidak valid' });
         }
 
-        // Simpan informasi pengguna dalam req.user
-        req.user = decoded; // menyimpan informasi pengguna dari token
-        next(); // Lanjutkan ke rute berikutnya
+        req.user = decoded; 
+        next();
     });
 };
 
 // Middleware untuk memverifikasi superuser
 exports.verifySuperuser = (req, res, next) => {
-    // Pastikan authenticateJWT telah dijalankan sebelumnya
     if (!req.user || req.user.role !== 'superuser') {
         return res.status(403).json({ message: 'Akses ditolak, hanya superuser yang dapat mengakses ini' });
     }
 
-    next(); // Lanjutkan ke rute berikutnya jika user adalah superuser
+    next(); 
 };
