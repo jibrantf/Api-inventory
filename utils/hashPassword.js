e5034f1e138dfbe4cc0d51ffe7ef651ad9dea170
jibrantf <brandozzzz05@gmail.com>
2024-12-30 08:48:58 +0700

    Update hashPassword.js

--- utils/hashPassword.js
+++ utils/hashPassword.js
@@ -1,9 +1,8 @@
 const bcrypt = require('bcrypt');
 
-// Fungsi untuk melakukan hashing password
 const hashPassword = async (password) => {
     try {
-        const saltRounds = 10; // Jumlah rounds untuk salt (semakin tinggi semakin aman, tapi juga lebih lambat)
+        const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         return hashedPassword;
     } catch (error) {   
